a5f07cfb33a7ff39b95b98467e7c41f905be7759
Daniel <danielmholland1@gmail.com>
2020-11-15 12:44:01 -0800

    ian did stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/IanLeraning.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+//import android.os.SystemClock;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.libraries.AutoLib;
+import org.firstinspires.ftc.teamcode.libraries.hardware.IanHardware;
+import org.firstinspires.ftc.teamcode.libraries.interfaces.ControllerLib;
+
+@TeleOp(name="Ian learning code lol")
+public class IanLeraning extends OpMode {
+
+}
